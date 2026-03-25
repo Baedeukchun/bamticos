f3aeac518159a8613a93a84e2cd1cf2e56cfe00c
User <bcmchaem@kentech.ac.kr>
2026-03-25 08:40:53 +0000

    test commit

--- 3/recap.cpp
+++ 3/recap.cpp
@@ -2,5 +2,30 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
-	int a, b, c, slm, num:
+	int a, b, c, sum, num;
+
+	if (argc != 3)
+	{
+		cerr << "Error! You must input two numbers!" << endl;
+		return 0;
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	if (a >= b)
+	{
+		cerr << "Error! The second argument must be bigger than the first argument" << endl;
+		return 0;
+	}
+
+	sum = 0;
+	for (num = a; num <= b; num++)
+		sum += num;
+
+	cout << "Sum:" << sum << endl;
+
+	return 0;
+
+}
 
